a7f2beaf74f19324392cd67a82d4e96ae3bb400b
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-19 12:43:04 +0100

    feat(layouts): create the main layout

---
+++ layouts/DashboardLayout.js
@@ -0,0 +1,30 @@
+export default function DashboardLayout(outlet) {
+	return `
+   	<aside class="menu">
+			<div class="logo"><img src="../assets/images/logo.svg" alt="logo" /></div>
+			<div class="menu__tabs">
+				<button class="menu__tab" data-path='/'>
+					<div class="icon"><img src="../assets/icons/dashboard.png" alt="dashboard icon" /></div>
+					<p class="">Tableau de bord</p>
+				</a>
+				<button class="menu__tab" data-path='/patients'>
+					<div class="icon"><img src="../assets/icons/patient.png" alt="dashboard icon" /></div>
+					<p class="">Patients</p>
+				</a>
+				<button class="menu__tab" data-path='/rendez-vous'>
+					<div class="icon"><img src="../assets/icons/calender.png" alt="dashboard icon" /></div>
+					<p class="">Rendez-vous</p>
+				</a>
+				<button class="menu__tab" data-path='/finances'>
+					<div class="icon"><img src="../assets/icons/finance.png" alt="dashboard icon" /></div>
+					<p class="">Finance</p>
+				</a>
+			</div>
+			<button class="logout-btn">
+				<div class="icon"><img src="../assets/icons/leave.png" alt="dashboard icon" /></div>
+				<p class="">Deconnexion</p>
+			</button>
+		</aside>
+      ${outlet || ""}
+   `;
+}
