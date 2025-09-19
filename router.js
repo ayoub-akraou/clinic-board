7329a8d6ea57722f0d890f915886b7ef9d65d5e6
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-19 12:48:33 +0100

    feat(router): create a dynamic SPA router

---
+++ router.js
@@ -0,0 +1,50 @@
+import DashboardLayout from "./layouts/DashboardLayout.js";
+import TableauDeBord from "./pages/TableauDeBord.js";
+import Patients from "./pages/Patients.js";
+import RendezVous from "./pages/RendezVous.js";
+import Finances from "./pages/Finances.js";
+import Login from "./pages/Login.js";
+import Register from "./pages/Register.js";
+
+const root = document.getElementById("root");
+
+const routes = {
+	"/": TableauDeBord(),
+	"/patients": Patients(),
+	"/rendez-vous": RendezVous(),
+	"/finances": Finances(),
+	"/login": Login(),
+	"/register": Register(),
+};
+
+function router() {
+	const path = location.pathname;
+
+	const page = routes[path] ? routes[path] : routes["/"];
+
+	root.innerHTML = DashboardLayout(page);
+	attachClickListener();
+}
+router();
+function navigateTo(e) {
+	const path = e.currentTarget.dataset.path;
+	console.log(e.currentTarget);
+	history.pushState(null, null, path);
+	router();
+}
+
+const menuTabs = document.querySelectorAll(".menu__tab");
+
+function attachClickListener() {
+	const menuTabs = document.querySelectorAll(".menu__tab");
+	menuTabs.forEach((tab) => {
+		tab.addEventListener("click", navigateTo);
+
+		console.log(tab);
+	});
+}
+
+// initial attachement
+attachClickListener();
+
+window.onpopstate = router;
